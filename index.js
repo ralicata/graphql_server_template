c9c8be253a8646e3c485101c8f36cdacdb74ff17
Roberto Alicata <roberto.alicata@gmail.com>
2017-10-13 10:03:16 +0200

    Add models directory

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { makeExecutableSchema } from "graphql-tools";
 
 import typeDefs from "./schema";
 import resolvers from "./resolvers";
+import models from "./models";
 
 const PORT = 3000;
 
@@ -15,7 +16,11 @@ const schema = makeExecutableSchema({
   resolvers
 });
 
-app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({ schema, context: { models } })
+);
 
 app.use(
   "/graphiql",
@@ -24,4 +29,4 @@ app.use(
   })
 );
 
-app.listen(PORT);
+models.sequelize.sync().then(() => app.listen(PORT));
---
+++ models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from "sequelize";
+const sequelize = new Sequelize(
+  "test_graphql_db",
+  "test_admin",
+  "secretpassword",
+  {
+    host: "localhost",
+    dialect: "postgres"
+  }
+);
+
+const db = {
+  User: sequelize.import("./user")
+};
+
+// Object.keys(db).forEach((modelName) => {
+//   if ('associate' in db[modelName]) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+export default db;
